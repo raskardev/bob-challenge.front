24e38170d32ea57bf68ae054a5604b01c747ade9
raskardev <alexcdev95@gmail.com>
2020-01-20 13:56:31 +0100

    feat(): add screens to App

--- src/App.js
+++ src/App.js
@@ -1,25 +1,18 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import Layout from "./components/Layout";
+import { Switch, Route } from "react-router-dom";
+
+import Home from "./screens/Home";
+import AddBag from "./screens/AddBag";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Layout>
+      <Switch>
+        <Route path='/new' exact component={AddBag} />
+        <Route path='/' exact component={Home} />
+      </Switch>
+    </Layout>
   );
 }
 
